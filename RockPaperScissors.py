bde7c40d107c6c0e7d2783e8785ccc81b218ade8
Arav PP <aravpp03@gmail.com>
2025-07-10 23:45:16 +0530

    score

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -4,9 +4,13 @@ emojis = {"r" : "🪨", "p" : "📃", "s" : "✂️"}
 
 choices = ("r", "p", "s")
 
+user_score = 0
+
+computer_score = 0
+
 while True:
 
-    user_choice = input("Rock, Paper, Scissors ?  (r/p/s): ").lower()
+    user_choice = input("Rock 🪨, Paper 📃, Scissors ✂️ ?  (r/p/s): ").lower()
 
     if user_choice not in choices:
 
@@ -31,13 +35,20 @@ while True:
 
         print("You Win")
 
+        user_score += 1
+
     else:
 
         print("You Lose")
 
+        computer_score += 1
+
+    print(f"Score : Your Score: {user_score}  -  Computer Score: {computer_score}")
 
     should_continue = input("Play?  (y / n): ").lower()
 
-    if should_continue == "n":
+    if should_continue not in ("y"or"yes"):
+
+        print("Thanks for Playing!")
 
         break
