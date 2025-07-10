f9cd1c4ea5d8bc4b336eda0c5b4df1c4d883c3f9
Arav PP <aravpp03@gmail.com>
2025-07-10 23:08:38 +0530

    user choice

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -3,3 +3,13 @@ import random
 imojis = {"r" : "🪨", "p" : "📃", "s" : "✂️"}
 
 choices = ("r", "p", "s")
+
+while True:
+
+    user_choice = input("Rock, Paper, Scissors ?  (r/p/s): ").lower()
+
+    if user_choice not in choices:
+
+        print("Invalid choice")
+
+        continue
